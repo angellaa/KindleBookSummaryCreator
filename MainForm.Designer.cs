216b8ccb9b754badd712f0500c3104e588bcdccb
Andrea Angella <angella.andrea@gmail.com>
2014-03-09 22:25:29 +0000

    Added project.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace KindleBookSummaryCreator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDetect = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtKindleFolder = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtOutputFolder = new System.Windows.Forms.TextBox();
+            this.btnOutputFolder = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDetect
+            // 
+            this.btnDetect.Location = new System.Drawing.Point(260, 36);
+            this.btnDetect.Name = "btnDetect";
+            this.btnDetect.Size = new System.Drawing.Size(75, 20);
+            this.btnDetect.TabIndex = 0;
+            this.btnDetect.Text = "Detect";
+            this.btnDetect.UseVisualStyleBackColor = true;
+            this.btnDetect.Click += new System.EventHandler(this.btnDetect_Click);
+            // 
+            // txtKindleFolder
+            // 
+            this.txtKindleFolder.Location = new System.Drawing.Point(12, 36);
+            this.txtKindleFolder.Name = "txtKindleFolder";
+            this.txtKindleFolder.Size = new System.Drawing.Size(242, 20);
+            this.txtKindleFolder.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Kindle Clipping File:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Output Folder:";
+            // 
+            // txtOutputFolder
+            // 
+            this.txtOutputFolder.Location = new System.Drawing.Point(12, 80);
+            this.txtOutputFolder.Name = "txtOutputFolder";
+            this.txtOutputFolder.Size = new System.Drawing.Size(242, 20);
+            this.txtOutputFolder.TabIndex = 4;
+            // 
+            // btnOutputFolder
+            // 
+            this.btnOutputFolder.Location = new System.Drawing.Point(260, 80);
+            this.btnOutputFolder.Name = "btnOutputFolder";
+            this.btnOutputFolder.Size = new System.Drawing.Size(75, 20);
+            this.btnOutputFolder.TabIndex = 3;
+            this.btnOutputFolder.Text = "Browse";
+            this.btnOutputFolder.UseVisualStyleBackColor = true;
+            this.btnOutputFolder.Click += new System.EventHandler(this.btnOutputFolder_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 118);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(323, 42);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 176);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtOutputFolder);
+            this.Controls.Add(this.btnOutputFolder);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtKindleFolder);
+            this.Controls.Add(this.btnDetect);
+            this.Name = "MainForm";
+            this.Text = "Kindle Book Summary Creator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDetect;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox txtKindleFolder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtOutputFolder;
+        private System.Windows.Forms.Button btnOutputFolder;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace KindleBookSummaryCreator
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DetectClippingsFile();
+
+            // Set the desktop as the default output folder
+            txtOutputFolder.Text = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\Summaries";
+        }
+
+        private void DetectClippingsFile()
+        {
+            // Automatically select the clipping file
+            var clippingFile = GetKindleClippingFile();
+
+            if (clippingFile != null)
+            {
+                txtKindleFolder.Text = clippingFile;
+            }
+        }
+
+        private string GetKindleClippingFile()
+        {
+            var drives = DriveInfo.GetDrives().Where(x => x.DriveType == DriveType.Removable);
+
+            foreach (var drive in drives)
+            {
+                string clippingPath = Path.Combine(drive.RootDirectory.FullName, @"documents\My Clippings.txt");
+
+                if (File.Exists(clippingPath))
+                {
+                    return clippingPath;
+                }
+            }
+
+            return null;
+        }
+
+        private void btnDetect_Click(object sender, EventArgs e)
+        {
+            DetectClippingsFile();
+        }
+
+        private void btnOutputFolder_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtOutputFolder.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            var streamReader = File.OpenText(txtKindleFolder.Text);
+
+            var summaries = new Dictionary<string, string>();
+ 
+            while (!streamReader.EndOfStream)
+            {
+                var bookTitle = streamReader.ReadLine();
+                var type = streamReader.ReadLine();
+
+                if (type.Contains("Highlight"))
+                {
+                    if (!summaries.ContainsKey(bookTitle))
+                        summaries[bookTitle] = "";
+                }
+
+                var line = streamReader.ReadLine();
+                var text = "";
+                while (line != "==========")
+                {
+                    text += line;
+                    line = streamReader.ReadLine();
+                }
+
+                if (type.Contains("Highlight"))
+                {
+                    summaries[bookTitle] += text + "\n";
+                }
+            }
+
+            var outputPath = txtOutputFolder.Text;
+
+            foreach (var summary in summaries)
+            {
+                File.WriteAllText(Path.Combine(outputPath, Sanitize(summary.Key) + ".txt"), summary.Value);
+            }
+
+            Process.Start(outputPath);
+        }
+
+        private static string Sanitize(string name)
+        {
+            var sb = new StringBuilder();
+
+            foreach (var c in name.Where(c => !Path.GetInvalidFileNameChars().Contains(c)))
+            {
+                sb.Append(c);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
